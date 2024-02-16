00293119d7c2f7b068cc9a5e04e097d67df8ff21
oksuriini <oskari.peiponen@hotmail.com>
2024-02-16 16:01:00 +0200

    Differentiated setting all envars, and getting one envar

--- src/envfile/envfile.go
+++ src/envfile/envfile.go
@@ -6,33 +6,70 @@ import (
 	"strings"
 )
 
-func ParseEnvFile(fileName string) {
-	content, err := os.ReadFile(fileName)
+// Parses environmental variables from file and sets them as envars
+func SetAllEnvars(fileName string) {
+	lines, err := parseFile(fileName)
 	if err != nil {
 		fmt.Println("error: ", err)
 		return
 	}
 
-	lines := strings.Split(string(content), "\n")
-
-	for ind, line := range lines {
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		envar := strings.Split(line, "=")
-
-		if len(envar) != 2 {
-			fmt.Printf("Invalid line at line %d\n", ind)
-			continue
-		}
-
-		key := strings.TrimSpace(envar[0])
-		value := strings.TrimSpace(envar[1])
-
+	for index, line := range lines {
+		key, value := parseLine(line, index)
 		err := os.Setenv(key, value)
 		if err != nil {
 			fmt.Printf("Error while setting envar %s, with value %s: %s", key, value, err)
 		}
 	}
+}
+
+// Parses environmental variables from file and returns the value of found key
+// If no key is found then returns empty string
+func GetOneEnvar(envName, fileName string) (string, error) {
+	lines, err := parseFile(fileName)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return "", err
+	}
+
+	for index, line := range lines {
+		key, value := parseLine(line, index)
+		if key == envName {
+			return value, nil
+		} else {
+			continue
+		}
+	}
+
+	err = fmt.Errorf("could not find envar %s", envName)
+	return "", err
+}
+
+// Parses given line and returns found key and value
+func parseLine(line string, index int) (key, value string) {
+	if line == "" || strings.HasPrefix(line, "#") {
+		return "", ""
+	}
+
+	envar := strings.Split(line, "=")
+
+	if len(envar) != 2 {
+		fmt.Printf("Invalid line at line %d\n", index)
+		return "", ""
+	}
+
+	key = strings.TrimSpace(envar[0])
+	value = strings.TrimSpace(envar[1])
+	return
+}
+
+// Parses file and returns slice of strings that hold all parsed lines
+func parseFile(fileName string) ([]string, error) {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(content), "\n")
+	return lines, nil
 }
